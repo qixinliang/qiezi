6d9bfbdf6894ddbd51f56c82c688e5831f54bfb6
qixinliang <qixinliang@6clue.com>
2017-11-15 17:29:35 +0800

    bug fix

--- application/home/controller/Category.php
+++ application/home/controller/Category.php
@@ -3,9 +3,22 @@
  * 分类控制器
  */
 namespace app\home\controller;
+use app\home\model\Kind as KindModel; 
 
 class Category{
 	public function index(){
-		return view('index');	
+
+		//获取所有的分类列表数据
+		$rows = KindModel::all();
+
+		$kind = [];
+		foreach($rows as $r){
+			$temp = [
+				'id'  => $r->id,
+				'name' => $r->name
+			];
+			$kind[] = $temp;
+		}
+		return view('index',['kind' => $kind]);
 	}
 } 
