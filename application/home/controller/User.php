e23324e9a664ada928bfd72d5c1373357afec1be
qixinliang <qixinliang@6clue.com>
2017-11-28 17:37:11 +0800

    bug fix

--- application/home/controller/User.php
+++ application/home/controller/User.php
@@ -3,6 +3,8 @@
  * 用户控制器
  */
 namespace app\home\controller;
+use app\home\model\User   as UserModel;
+use app\home\model\Ticket as TicketModel;
 class User{
 	public function recharge(){
 		return view('recharge');	
@@ -10,10 +12,33 @@ class User{
 
 	//我的个人信息
 	public function userinfo(){
-		return view('userinfo');	
+		//用户登陆后，能知道他的openid
+		$row = UserModel::get([
+			'openid' => 1,
+		]);
+		if(!isset($row) || empty($row)){
+			return json([
+				'error_code' => -1,
+				'error_msg'  => '用户未找到'
+			]);
+		}
+
+		$ticket = TicketModel::get([
+			'id' => $row->uid
+		]);
+
+		$bookTicket = 0;
+		if(isset($ticket) && !empty($ticket)) {
+			$bookTicket = $ticket->book_ticket;
+		}
+
+		return view('userinfo',[
+			'user' => $row,
+			'book_ticket' => $bookTicket
+		]);
 	}
 
 	public function help(){
-		return view('help');	
+		return view('help');
 	}
 }
