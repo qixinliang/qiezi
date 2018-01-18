5603dcdd49c3b8928bcdb3b433c218e6bc07b855
qixinliang <qixinliang@6clue.com>
2018-01-18 18:56:07 +0800

    bug fix

--- public/yjcard/config.php
+++ public/yjcard/config.php
@@ -1,5 +1,14 @@
 <?php
 $settings = [
-	'id'		=> '15014926438050849',
-	'secret'	=> 'e6b85fe4574bebbace4ba3eccd4744b9'
+	'id'			=> '15014926438050849',
+	'secret'		=> 'e6b85fe4574bebbace4ba3eccd4744b9'
+	'amount'		=> 1,
+	'notice_url'	=> 'http://w.jxyx.net/yjcard/notify.php',
+	'back_url'		=> 'http://w.jxyx.net/yjcard/return.php',
+];
+$dsn = [
+	'host'		=> '127.0.0.1',
+	'db'		=> 'qiezi',
+	'username'	=> 'root',
+	'password'	=> '123456'
 ];
