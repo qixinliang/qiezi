e5d1537813a5996f8238e3f852921407b4af0302
qixinliang <qixinliang@6clue.com>
2018-01-23 14:36:22 +0800

    sql update

--- public/yjcard/game.sql
+++ public/yjcard/game.sql
@@ -11,33 +11,17 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 23/01/2018 14:01:25
+ Date: 23/01/2018 14:35:39
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for order
+-- Table structure for auth
 -- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_no` varchar(50) DEFAULT NULL,
-  `account` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `create_time` int(11) DEFAULT NULL,
-  `update_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for order_backup
--- ----------------------------
-DROP TABLE IF EXISTS `order_backup`;
-CREATE TABLE `order_backup` (
+DROP TABLE IF EXISTS `auth`;
+CREATE TABLE `auth` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `account` varchar(50) DEFAULT NULL COMMENT '账号',
@@ -48,5 +32,18 @@ CREATE TABLE `order_backup` (
   `update_time` int(11) DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(100) DEFAULT NULL COMMENT '账号',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  `name` varchar(100) DEFAULT NULL COMMENT '角色名',
+  `create_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
