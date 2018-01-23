1d147eb16458d778912ff72fdd65db89a255f74c
qixinliang <qixinliang@6clue.com>
2018-01-23 15:53:24 +0800

    bug fix

--- public/yjcard/game.sql
+++ public/yjcard/game.sql
@@ -18,10 +18,10 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for auth
+-- Table structure for order 
 -- ----------------------------
-DROP TABLE IF EXISTS `auth`;
-CREATE TABLE `auth` (
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `account` varchar(50) DEFAULT NULL COMMENT '账号',
@@ -34,16 +34,16 @@ CREATE TABLE `auth` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for order
+-- Table structure for auth
 -- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `auth`;
+CREATE TABLE `auth` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `account` varchar(100) DEFAULT NULL COMMENT '账号',
   `password` varchar(100) DEFAULT NULL COMMENT '密码',
   `name` varchar(100) DEFAULT NULL COMMENT '角色名',
   `create_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
